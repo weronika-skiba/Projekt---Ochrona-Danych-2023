2a9ebab018a72b584c0188523e719263278e723e
Weronika <jelly.beans.2369207@gmail.com>
2023-01-14 18:33:26 +0100

    Kolejna wersja

--- app/hello.py
+++ app/hello.py
@@ -1,6 +1,7 @@
 import string
 from flask import Flask, render_template, request, make_response, redirect
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_wtf.csrf import CSRFProtect
 import markdown
 from collections import deque
 from passlib.hash import argon2
@@ -10,22 +11,27 @@ import bleach
 import random
 import re
 
-app = Flask(__name__)
-
+csrf = CSRFProtect()
 login_manager = LoginManager()
+DATABASE = "./sqlite3.db"
+app = Flask(__name__)
+app.secret_key = "206363ef77d567cc511df5098695d2b85058952afd5e2b1eecd5aed981805e60"
+csrf.init_app(app)
 login_manager.init_app(app)
 
-app.secret_key = "206363ef77d567cc511df5098695d2b85058952afd5e2b1eecd5aed981805e60"
+@app.after_request
+def add_security_headers(response):
+    response.headers['Content-Security-Policy']="default-src 'self'; img-src * data:;"
+    return response
 
-DATABASE = "./sqlite3.db"
 def restore_db():
     print("[*] Init database!")
     sq = sqlite3.connect(DATABASE)
     sql = sq.cursor()
-    sql.execute("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, username VARCHAR(32) NOT NULL, password VARCHAR(128) NOT NULL);")
+    sql.execute("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, username VARCHAR(32) NOT NULL, email VARCHAR(128) NOT NULL, password VARCHAR(1024) NOT NULL);")
     sql.execute("CREATE TABLE IF NOT EXISTS login_attempts (id INTEGER PRIMARY KEY, username VARCHAR(32) NOT NULL, count INTEGER DEFAULT 0);")
     sql.execute(
-        "CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY, username VARCHAR(32) NOT NULL, note VARCHAR(256) NOT NULL, pictureURL VARCHAR(256), isPublic INTEGER DEFAULT 0, isProtected INTEGER DEFAULT 0, password VARCHAR(128));")
+        "CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY, username VARCHAR(32) NOT NULL, note VARCHAR(1024) NOT NULL, isPublic INTEGER DEFAULT 0, isProtected INTEGER DEFAULT 0, password VARCHAR(128), datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP);")
     sq.commit()
     sq.close()
 
@@ -111,6 +117,8 @@ def register():
         sql = db.cursor()
         username = request.form.get('username')
         password = request.form.get('password')
+        email = request.form.get('email')
+        re_email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
         lowercase_count = 0
         uppercase_count = 0
         punctuation_count = 0
@@ -118,6 +126,12 @@ def register():
         sql_query = "SELECT count(*) FROM user WHERE username = ?"
         sql.execute(sql_query, (username,))
         result = sql.fetchone()[0]
+        if len(username) <= 0:
+            return "Musisz podać nazwę użytkownika."
+        if len(username) > 32:
+            return "Podana nazwa użytkownika jest zbyt długa."
+        if not (re.fullmatch(re_email, email)):
+            return "Podano nieprawidłowy adres email."
         if result > 0:
             return "Wybierz inną nazwę użytkownika."
         if len(password) < 8:
@@ -137,8 +151,8 @@ def register():
             hash = argon2.using(
                 salt=bytes(''.join(random.choices(population=string.ascii_letters, k=random.randint(10, 15))),
                            'ascii')).hash(password)
-            sql_query = "INSERT INTO user (username, password) VALUES (?, ?);"
-            sql.execute(sql_query, (username, hash, ))
+            sql_query = "INSERT INTO user (username, email, password) VALUES (?, ?, ?);"
+            sql.execute(sql_query, (username, email, hash, ))
             db.commit()
             return redirect('/')
 
@@ -149,12 +163,19 @@ def new_password():
     if request.method == 'POST':
         username = request.form.get('username')
         email = request.form.get('email')
+        db = sqlite3.connect(DATABASE)
+        sql = db.cursor()
+        sql_query = "SELECT count(*) FROM user WHERE username = ? and email = ?"
+        sql.execute(sql_query, (username,email,))
+        result = sql.fetchone()[0]
         re_email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-        if (re.fullmatch(re_email, email)):
-            print(f"Użytkownik poprosił o zmianę hasła, wysłałabym mu link: https://link_do_zmiany_hasla.pl na adres e-mail: '{email}'")
+        if not (re.fullmatch(re_email, email)):
+            return "Podano nieprawidłowy adres email."
+        if result == 1:
+            print(f"Użytkownik '{username}' poprosił o zmianę hasła, wysłałabym mu link: https://link_do_zmiany_hasla.pl na adres e-mail: '{email}'")
             return "Link do zmiany hasła został wysłany."
         else:
-            return "Podano nieprawidłowy adres email."
+            return "Podano nieprawidłowe dane."
 
 
 @app.route("/hello", methods=['GET', 'POST'])
@@ -164,7 +185,7 @@ def hello():
         username = current_user.id
         db = sqlite3.connect(DATABASE)
         sql = db.cursor()
-        sql_query = "SELECT id, isPublic, isProtected FROM notes WHERE username == ? OR isPublic = 1"
+        sql_query = "SELECT id, isPublic, isProtected, datetime, username FROM notes WHERE username == ? OR isPublic = 1"
         sql.execute(sql_query, (username,))
         notes = sql.fetchall()
         return render_template("hello.html", username=username, notes=notes)
@@ -179,17 +200,11 @@ def hello():
         elif "submit_unpublish" in request.form:
             sql_query = "UPDATE notes SET isPublic = 0 WHERE username = ? AND id = ?"
             sql.execute(sql_query, (username, note_id,))
-        elif "submit_protect" in request.form:
-            sql_query = "UPDATE notes SET isProtected = 1 WHERE username = ? AND id = ?"
-            sql.execute(sql_query, (username, note_id,))
-        elif "submit_not_protect" in request.form:
-            sql_query = "UPDATE notes SET isProtected = 0 WHERE username = ? AND id = ?"
-            sql.execute(sql_query, (username, note_id,))
         db.commit()
         db.close()
         db = sqlite3.connect(DATABASE)
         sql = db.cursor()
-        sql_query = "SELECT id, isPublic, isProtected FROM notes WHERE username == ? OR isPublic = 1"
+        sql_query = "SELECT id, isPublic, isProtected, datetime, username FROM notes WHERE username == ? OR isPublic = 1"
         sql.execute(sql_query, (username,))
         notes = sql.fetchall()
         return render_template("hello.html", username=username, notes=notes)
@@ -198,17 +213,15 @@ def hello():
 @app.route("/render", methods=['POST'])
 @login_required
 def render():
-    picture_url = request.form.get("picture_url")
-    url = request.form.get("url")
-    md = bleach.clean(request.form.get("markdown", ""), tags=['a','b','i','h1','h2','h3','h4','h5','br'])
+    md = bleach.clean(request.form.get("markdown", ""), tags=['a','p','b','i','h1','h2','h3','h4','h5','br'])
     rendered = markdown.markdown(md)
     username = current_user.id
     db = sqlite3.connect(DATABASE)
     sql = db.cursor()
-    sql_query = "INSERT INTO notes (username, note, pictureURL) VALUES (?, ?, ?);"
-    sql.execute(sql_query, (username, rendered, picture_url,))
+    sql_query = "INSERT INTO notes (username, note) VALUES (?, ?);"
+    sql.execute(sql_query, (username, rendered,))
     db.commit()
-    return render_template("markdown.html", rendered=rendered, picture_url=picture_url)
+    return render_template("markdown.html", rendered=rendered)
 
 
 @app.route("/render/<rendered_id>")
@@ -216,13 +229,13 @@ def render():
 def render_old(rendered_id):
     db = sqlite3.connect(DATABASE)
     sql = db.cursor()
-    sql_query = "SELECT username, note, pictureURL FROM notes WHERE id == ?"
+    sql_query = "SELECT username, note FROM notes WHERE id == ?"
     sql.execute(sql_query, (rendered_id,))
     try:
-        username, rendered, picture_url = sql.fetchone()
+        username, rendered = sql.fetchone()
         if username != current_user.id:
             return "Access to note forbidden", 403
-        return render_template("markdown.html", rendered=rendered, picture_url=picture_url)
+        return render_template("markdown.html", rendered=rendered)
     except:
         return "Note not found", 404
 
